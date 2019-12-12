baef2013d25a211aca117710f2ab8fa746dfd025
asd44555779 <asd44555779@163.com>
2019-12-12 09:15:37 +0800

    dev

--- data/data.py
+++ data/data.py
@@ -19,7 +19,12 @@ class Data():
 
     def mk_data_dir(self, dir):
         path = os.path.join(os.path.join(os.getcwd(), self.__root_path), dir)
-        os.makedirs(path)
+        if os.path.exists(path) == False :
+            os.makedirs(path)
+
+    def check_data(self, dir, name):
+        path = os.path.join(os.path.join(os.path.join(os.getcwd(), self.__root_path), dir), name)
+        return os.path.exists(path)
 
     def store_data(self, dir, name, df):
         path = os.path.join(os.path.join(os.path.join(os.getcwd(), self.__root_path), dir), name)
@@ -32,3 +37,9 @@ class Data():
         time_datetime = datetime.datetime.strptime(str_date, self.__TIME_FMT)
         time_prevday = time_datetime - datetime.timedelta(days = 1)
         return time_prevday.strftime(self.__TIME_FMT)
+
+    def compare_time(self, time1, time2):
+        #dtime1 = datetime.datetime.strptime(time1, self.__TIME_FMT)
+        #dtime2 = datetime.datetime.strptime(time2, self.__TIME_FMT)
+        #print("cmp", time1, time2, "date time", dtime1, dtime2)
+        return time1 == time2
--- main.py
+++ main.py
@@ -1,31 +1,57 @@
 from stocks.stock_ts import *
 from data.data import *
+import sys
 
-def get_all_and_store(data, stock, str_day, df):
+def get_all_and_store(data, stock, str_day, df, cover):
     code_list = stock.get_code_list(df)
+    store_cnt = 0
     for code in code_list:
+        write = False
+        if data.check_data(str_day ,code+".csv"):
+            if cover:
+                write = True
+        if write == False:
+            continue
         df_code = stock.daily_detail(str_day, code)
-        data.store_data(str_day, code+".csv", df_code)
-    return code_list
+        data.store_data(str_day, code+".csv", df_code, cover)
+        store_cnt = store_cnt + 1
+    return code_list, store_cnt
 
-def get_daily_and_store(data, stock, str_day):
+def get_daily_and_store(data, stock, str_day, cover):
     df = stock.daily(str_day)
     data.mk_data_dir(str_day)
-    data.store_data(str_day ,"daily.csv", df)
+    write = False
+    if data.check_data(str_day ,"daily.csv"):
+        if cover:
+            write = True
+    if write :
+        data.store_data(str_day ,"daily.csv", df, cover)
     return df
 
 if __name__ == "__main__":
     stock = Stock()
     data = Data()
     str_day = data.today()
+    cover = False
+    all_detail = False
+    code_list = []
+    store_cnt = 0
+    if sys.argv[4] == "1" :
+        all_detail = True
+    if sys.argv[3] == "1" :
+        cover = True
+    if sys.argv[1] != "-" :
+        str_day = sys.argv[1]
+    end_day = sys.argv[2]
+    print("run begin day:", str_day, " to day:", end_day, " cover:", cover)
     while True:
+        if data.compare_time(str_day, end_day) == True:
+            break
         if stock.is_trade_date(str_day):
-            if data.check_data_dir(str_day):
-                print(str_day, " exists quit")
-                break
-            df = get_daily_and_store(data, stock, str_day)
-            code_list = get_all_and_store(data, stock, str_day, df)
-            print(str_day, " write csv all ", len(code_list), " codes")
+            df = get_daily_and_store(data, stock, str_day, cover)
+            if all_detail:
+                code_list, store_cnt = get_all_and_store(data, stock, str_day, df, cover)
+            print(str_day, " write csv all ", len(code_list), " codes store count:", store_cnt)
         else:
             print(str_day, " is not trade day")
         str_day = data.prevday(str_day)
